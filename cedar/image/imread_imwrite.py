fbf38be1d4dde2a85b2a3f2a9a34c3ed2029d4ab
张松 <s.zhang.cedar@gmail.com>
2024-04-22 13:38:22 +0800

    优化imread注释

--- cedar/image/imread_imwrite.py
+++ cedar/image/imread_imwrite.py
@@ -7,7 +7,9 @@ from cedar.utils.tools import split_filename
 
 def imread(image_path: str, flag: int = cv2.IMREAD_COLOR) -> np.ndarray:
     """读取图片、兼容中文路径、 cv2.IMREAD_COLOR 始终将图像转换为3通道BGR彩色图像。
-
+        cv2.IMREAD_COLOR：这是默认值，表示图像将以彩色方式读取，即每个像素将由三个通道（BGR）表示。
+        cv2.IMREAD_GRAYSCALE：这个标志表示图像将以灰度方式读取，即每个像素将只有一个通道，表示其亮度信息。
+        cv2.IMREAD_UNCHANGED：这个标志表示图像将包括其 alpha 通道（如果存在）一起读取。这对于处理带有透明度的图像（如PNG格式）是必要的。
     Args:
         image_path (str): 图片路径
         flag (int, optional): 图片读取标志。 Defaults to cv2.IMREAD_COLOR,详情见cv2.IMREAD_*。
