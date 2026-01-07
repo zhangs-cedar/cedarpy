36f681cd9eaf096b2405436c14a62717f26290cf
zhangsong <s.zhang.cedar@gmail.com>
2026-01-07 22:53:18 +0800

    删除多个不再使用的测试文件和配置文件，以简化代码库并提升可维护性。

--- cedar/utils/test/test.py
+++
@@ -1,9 +0,0 @@
-import time
-from cedar.utils.logger import init_logger
-from cedar.utils import logger
-
-logger.init_logger()
-logger.info('hello')
-time.sleep(1)
-logger.debug('world')
-logger.warning('warning')
--- cedar/utils/test/test_utils.py
+++
@@ -1,9 +0,0 @@
-from cedar.utils import try_except
-
-
-@try_except
-def test_try_except():
-    raise
-
-
-test_try_except()
